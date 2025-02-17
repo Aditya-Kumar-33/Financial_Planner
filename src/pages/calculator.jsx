13c8493d8cbdbfa79c4d29e9df9e72573883bafd
Adita Kumar <aditya22033@iiitd.ac.in>
2025-02-18 00:34:07 +0530

    buttons

--- src/pages/calculator.jsx
+++ src/pages/calculator.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import DonutChart from '../components/DonutChart'
-import { calculateRequiredInvestment } from '../Functions/calculateRequiredInvestment '
+import { calculateRequiredInvestment } from '../Functions/CalculateRequiredInvestment'
 import { calculateInvestment } from '../Functions/CalculateInvestment'
 
 const Calculator = () => {
-  const [activeButton, setActiveButton] = useState('target'); // 'target' or 'growth'
+  const [activeButton, setActiveButton] = useState("target"); // 'target' or 'growth'
   return (
     <>
       <div className='h-screen w-screen grid grid-rows-[5%,95%]'>
@@ -12,8 +12,10 @@ const Calculator = () => {
         <div className='h-full w-full bg-white flex justify-center items-center gap-[30px]'>
           <button
             type="button"
-            onClick={() => setActiveButton('target')}
-            className={`text-white ${activeButton === 'target' ? 'bg-blue-700 hover:bg-blue-800' : 'bg-white border-2 border-blue-700 text-blue-700'} 
+            onClick={() => {setActiveButton("target") 
+              console.log(activeButton)
+            }}
+            className={`text-white ${activeButton === "target" ? 'bg-blue-700 hover:bg-blue-800' : 'bg-white border-2 border-blue-700 text-blue-700'} 
                         focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-[11.5px] me-2 mb-2 
                         dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`}
           >
@@ -22,8 +24,11 @@ const Calculator = () => {
 
           <button
             type="button"
-            onClick={() => setActiveButton('growth')}
-            className={`text-white ${activeButton === 'growth' ? 'bg-blue-700 hover:bg-blue-800' : 'bg-white border-2 border-blue-700 text-blue-700'} 
+            onClick={() => {setActiveButton("growth")
+              console.log(activeButton)
+            }
+            }
+            className={`text-white ${activeButton === "growth" ? 'bg-blue-700 hover:bg-blue-800' : 'bg-white border-2 border-blue-700 text-blue-700'} 
                         focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-[10px] me-2 mb-2 
                         dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`}
           >
