5f75dfd582d9a8f0a8df7a0c5b59c959987201c1
Adita Kumar <aditya22033@iiitd.ac.in>
2025-03-09 13:32:47 +0530

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,37 @@ import BackGround from "../components/BackGround";
 
 const Home = () => {
   return (
-    <div className="relative">
+    <div className="relative h-screen">
+
       <BackGround />
-      <div className="absolute inset-0">
-        <h1 className="text-white text-3xl font-bold">Finance Management Made Simple</h1>
-        <h1 className="text-white text-3xl font-bold">
+
+      {/* Container over bg */}
+      <div className="absolute inset-0 flex flex-col h-[200vh]">
+        
+        <div className="flex flex-col items-center text-center space-y-6 mt-[25vh]">
+          <h1 className="text-white text-5xl font-bold font-manrope">
+            Finance Management Made Simple
+          </h1>
+          <h1 className="text-white text-3xl font-bold font-spaceGrotesk">
             Make better financial decisions with our expense tracker, investment planner, and cost calculator.
-        </h1>
-        <div className="w-1/4 h-1/4 rounded-2xl bg-gradient-to-b from-[#111125] to-transparent"></div>
+          </h1>
+        </div>
+
+        {/* Bottom Section */}
+        <div className="relative h-full w-full">
+            <div className="absolute right-[10%] top-[10%] w-1/3 h-1/4 rounded-2xl bg-gradient-to-b from-[#111125] to-transparent shadow-[0_-4px_10px_rgba(255,255,255,0.3)] text-white">
+                Hey
+            </div>
+            <div className="absolute left-[10%] top-[25%] w-1/3 h-1/4 rounded-2xl bg-gradient-to-b from-[#111125] to-transparent shadow-[0_-4px_10px_rgba(255,255,255,0.3)] text-white">
+                Hey
+            </div>
+            <div className="absolute right-[10%] top-[40%] w-1/3 h-1/4 rounded-2xl bg-gradient-to-b from-[#111125] to-transparent shadow-[0_-4px_10px_rgba(255,255,255,0.3)] text-white">
+                Hey
+            </div>
+            <div className="absolute left-[10%] top-[55%] w-1/3 h-1/4 rounded-2xl bg-gradient-to-b from-[#111125] to-transparent shadow-[0_-4px_10px_rgba(255,255,255,0.3)] text-white">
+                Hey
+            </div>
+        </div>
       </div>
     </div>
   );
